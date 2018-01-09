bd75a3d6b5139e69b8b4c57b5d5b80622810668a
Jeff Wagner <jwagner@thoughtworks.com>
2018-01-08 21:14:46 -0600

    WIP: add wishlist component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
+import NewWishlist from './NewWishlist/NewWishlist';
 
 class App extends Component {
+  state = {
+    createNewWishlist: false
+  }
+
+  createWishlistHandler = () => {
+    const doesShow = this.state.createNewWishlist;
+    this.setState({createNewWishlist: !doesShow})
+  }
+
   render() {
+    let newWishlist = null;
+    let message = 'Create a new wishlist!';
+
+    if (this.state.createNewWishlist) {
+      message = 'Remove wishlist';
+      newWishlist = (
+        <div>
+          <NewWishlist/>
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to Wishlist App!</h1>
         </header>
+        <button onClick={this.createWishlistHandler}>{message}</button>
+        {newWishlist}
       </div>
     );
   }
